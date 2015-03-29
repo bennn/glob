07c01f6f5f576146a6a719b09b493917c9000bf4
ben <types@ccs.neu.edu>
2015-03-29 18:26:51 -0400

    ** added info.rkt

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "glob")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define pkg-desc "Unix-style globbing")
+(define version "0.0")
+(define pkg-authors '(ben))
