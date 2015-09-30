ab016d1f683bae490dd86bc1f33481b004356611
ben <types@ccs.neu.edu>
2015-09-30 14:34:12 -0400

    ** update info.rkt with cover build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "glob")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("cover" "cover-coveralls" "scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Unix-style globbing")
 (define version "0.0")
 (define pkg-authors '(ben))
